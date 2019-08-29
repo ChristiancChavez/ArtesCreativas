be62643aa63b64a4923e7fda78c7fc916d47997d
Christian Chavez <christiancchavez10@hotmail.com>
2019-08-29 09:57:51 -0500

    add some styles and fix some structure of product's component

--- src/components/Nav/Products/products.js
+++ src/components/Nav/Products/products.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styles from './products.module.scss';
 
 const products = () => (
-    <section className={styles.background}>
-        <div className={styles.products}>
-            <h2 className={styles.title}>Productos</h2>
-            <h2 className={styles.product}>Cuadros</h2>
-            <h2 className={styles.product}>Cojines</h2>
-            <h2 className={styles.product}>Cobijas</h2>
-            <h2 className={styles.product}>Cortinas</h2>
-        </div>
+    <section className={styles.products}>
+        <h2 className={styles.title}>Productos</h2>
+        <div className={styles.product}>Cuadros y Repisas</div>
+        <div className={styles.product}>Cojines</div>
+        <div className={styles.product}>Cobijas</div>
+        <div className={styles.product}>Bustos</div>
     </section>
 )
 export default products;
